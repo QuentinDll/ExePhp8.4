88bf91e6b85713f2bd26aed22f08b3570912594f
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 09:47:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 8</title>
+</head>
+<body>
+  <p>
+    <?php
+    // declaration de la variable qui contient array
+    $numb = array(10, 20, 30, 40, 50);
+    // appel de la variable
+    echo array_sum($numb);
+    ?>
+  </p>
+</body>
+</html>
